2a9aeef00ffaeb82a1591bff2deaf80374ca22fe
wuq <793801814@qq.com>
2019-08-11 22:14:08 +0800

    添加项目文件。

---
+++ LeetCode.Algorithm/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LeetCode.Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1.两数之和
+            int[] input = new int[] { 3, 2, 4 };
+            int[] output = TwoSumSolution.TwoSum(input, 6);
+            #endregion
+
+        }
+    }
+}
---
+++ LeetCode.Algorithm/TwoSum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Algorithm
+{
+    public class TwoSumSolution
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] ret = new int[2];
+            //nums = nums.OrderBy(it => it).ToArray();
+            for (int i = 0; i < nums.Count() - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Count(); j++)
+                {
+
+                    if ((target - nums[i]).Equals(nums[j]))
+                    {
+                        ret[0] = i;
+                        ret[1] = j;
+                        return ret;
+                    }
+                }
+            }
+            return ret;
+        }
+    }
+}
